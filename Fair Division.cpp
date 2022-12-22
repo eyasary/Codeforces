5eed4a96816a3c398766f168841758f699522199
sari-eya <119814391+eyasary@users.noreply.github.com>
2022-12-22 22:45:03 +0100

    Add files via upload

--- Fair Division.cpp
+++ Fair Division.cpp
@@ -2,26 +2,36 @@
 using namespace std;
 int main()
 {
-    int t,n,s=0,n1=0,n2=0;
+    int t,n,s=0;
     cin>>t;
     while(t--)
     {
+        int n1=0,n2=0;
         cin>>n;
         int t1[n];
         for(int i=0;i<n;i++)
             {
                 cin>>t1[i];
-                s+=t1[i];
                 if(t1[i]==1)
                     n1++;
                 else
                     n2++;
             }
-            n2*=2;
-            if(n1==n2 || n1%2==0 || s%2==0)
+            if(n2%2!=0)
+            {
+                if(n1%2==0 && n1!=0)
+                    cout<<"YES"<<endl;
+                else
+                    cout<<"NO"<<endl;
+            }
+            else
+            {
+                if(n1%2==0 && n2%2==0)
                 cout<<"YES"<<endl;
             else
                 cout<<"NO"<<endl;
+            }
+
 
 
 
