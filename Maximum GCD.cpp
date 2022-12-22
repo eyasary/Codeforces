4cf3f3377ed76d74c3b6e088d00f8cff1f489942
sari-eya <119814391+eyasary@users.noreply.github.com>
2022-12-22 21:35:37 +0100

    Add files via upload

---
+++ Maximum GCD.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {   int n,n1,maxi=1,j=0;
+        cin>>n;
+        cout<<n/2<<endl;
+    }
+}
